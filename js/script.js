ff79c2f8d1222dc02bf04c9afabe404d2f15df08
bournecreative <bournecreative@gmail.com>
2017-01-26 16:55:06 -0800

    added board marking functionality to gameboard, need to develop win condition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var player_one = true;
+var player_two = false;
+
 //On page load, build game board
 function buildGameBoard() {
   for (var i=0; i<3; i++) {
@@ -8,6 +11,26 @@ function buildGameBoard() {
   }
 }
 
+//Mark board when user clicks
+function selectBoardSquare(){
+  $('.square').on('click', markBoardSquare);
+}
+
+function markBoardSquare(){
+  if (player_one){
+    $(this).off('click');
+    $(this).css("background-color","#1C34FF");
+    player_one = false;
+    player_two = true;
+  }else if(player_two){
+    $(this).off('click');
+    $(this).css("background-color","#FF4476");
+    player_two = false;
+    player_one = true;
+  }
+}
+
 $(document).ready(function(){
   buildGameBoard();
+  selectBoardSquare();
 });
